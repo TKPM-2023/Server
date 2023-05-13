0b3ebd3d073d48b40b96e04c98d397cdbbd9dd73
Quang (Waang) <81631701+orgball2608@users.noreply.github.com>
2023-05-13 15:46:06 +0700

    Repush

---
+++ src/main/java/hcmus/JwtAuthServiceApp.java
@@ -0,0 +1,31 @@
+package hcmus;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hcmus.service.UserService;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class JwtAuthServiceApp implements CommandLineRunner {
+
+  final UserService userService;
+
+  public static void main(String[] args) {
+    SpringApplication.run(JwtAuthServiceApp.class, args);
+  }
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+  @Override
+  public void run(String... params) throws Exception {
+  }
+
+}
